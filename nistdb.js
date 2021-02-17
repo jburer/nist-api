5cbb90e7ada9c0c8297a517f0564f89150f6a99e
jburer <jburer@gmail.com>
2021-02-17 10:55:37 -0500

    .initial

--- nistdb.js
+++ nistdb.js
@@ -1,6 +1,6 @@
-var publicationRoute = require("../nistdata/data/Publications/v1/Publications.json");
-var businessroleRoute = require("../nistdata/data/Roles/v1/Roles.json");
-var minimumrequirementRoute = require("../nistdata/data/FIPS200/v1/FIPS200.json");
+var publicationRoute = require("./data/Publications/v1/Publications.json");
+var businessroleRoute = require("./data/Roles/v1/Roles.json");
+var minimumrequirementRoute = require("./data/FIPS200/v1/FIPS200.json");
 
 module.exports = function () {
   return {
