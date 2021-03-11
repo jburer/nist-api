e8335c730464cef29141d45ca427e7cdac913935
jburer <jburer@gmail.com>
2021-03-11 17:27:54 -0500

    Route for Glossary

--- nistdb.js
+++ nistdb.js
@@ -1,11 +1,13 @@
 var publicationRoute = require("./data/Publications/v1/Publications.json");
 var businessroleRoute = require("./data/Roles/v1/Roles.json");
 var minimumrequirementRoute = require("./data/FIPS200/v1/FIPS200.json");
+var glossaryRoute = require("./data/Glossary/v1/Glossary.json");
 
 module.exports = function () {
   return {
     Publications: publicationRoute,
     Roles: businessroleRoute,
-    MinimumRequirements: minimumrequirementRoute
+    MinimumRequirements: minimumrequirementRoute,
+    Glossary: glossaryRoute
   };
 };
