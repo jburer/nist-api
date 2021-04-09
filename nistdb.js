c321c872c802f6bca037ab927a4cf0e19e8f4211
jburer <jburer@gmail.com>
2021-04-09 07:56:10 -0400

    Added FIPS 199

--- nistdb.js
+++ nistdb.js
@@ -1,13 +1,15 @@
 var publicationRoute = require("./data/Publications/v1/Publications.json");
 var businessroleRoute = require("./data/Roles/v1/Roles.json");
-var minimumrequirementRoute = require("./data/FIPS200/v1/FIPS200.json");
 var glossaryRoute = require("./data/Glossary/v1/Glossary.json");
+var FIPS199Route = require("./data/FIPS199/v1/FIPS199.json");
+var FIPS200Route = require("./data/FIPS200/v1/FIPS200.json");
 
 module.exports = function () {
   return {
     Publications: publicationRoute,
     Roles: businessroleRoute,
-    MinimumRequirements: minimumrequirementRoute,
-    Glossary: glossaryRoute
+    Glossary: glossaryRoute,
+    MinimumRequirements: FIPS200Route,
+    FIPS199: FIPS199Route
   };
 };
